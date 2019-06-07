847b3133e9190bfc2dcf34a2d4a0f06e723cc224
Antonio Rodriguez <antonio.santiesteban@leanovate.de>
2019-06-07 17:52:48 +0200

    SPOTBox-3: fallback the bundler version and fix deploy script.

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,15 @@
 echo "Building App..."
 sbt test & sbt fullOptJS::webpack
 
-USER = spoterme-spotbox
-APP = spotbox
-
 echo "Copying the App Bundle to public dir..."
-cp target/scala-2.12/scalajs-bundler/main/${APP}-opt-bundle.js public/spotbox/js/
+cp target/scala-2.12/scalajs-bundler/main/spotbox-opt-bundle.js public/spotbox/js/
 
 echo "Uploading the App to the Solid Server..."
-curl --cookie "connect.sid=${SOLID_COOKIE}" --upload-file index.html https://${USER}.solid.community/index.html
+curl --cookie "connect.sid=${SOLID_COOKIE}" --upload-file index.html https://spotbox.solid.community/index.html
 echo ":-)"
-curl --cookie "connect.sid=${SOLID_COOKIE}" --upload-file public/${APP}/css/app.css https://${USER}.solid.community/public/${APP}/css/app.css
+curl --cookie "connect.sid=${SOLID_COOKIE}" --upload-file public/spotbox/css/app.css https://spotbox.solid.community/public/spotbox/css/app.css
 echo ":-)"
-curl --cookie "connect.sid=${SOLID_COOKIE}" --upload-file public/${APP}/images/logo.png https://${USER}.solid.community/public/${APP}/images/logo.png
+curl --cookie "connect.sid=${SOLID_COOKIE}" --upload-file public/spotbox/images/logo.png https://spotbox.solid.community/public/spotbox/images/logo.png
 echo ":-)"
-curl --cookie "connect.sid=${SOLID_COOKIE}" --upload-file public/${APP}/js/${APP}-opt-bundle.js https://${USER}.solid.community/public/${APP}/js/${APP}-opt-bundle.js
+curl --cookie "connect.sid=${SOLID_COOKIE}" --upload-file public/spotbox/js/spotbox-opt-bundle.js https://spotbox.solid.community/public/spotbox/js/spotbox-opt-bundle.js
 echo "Done :-)"
