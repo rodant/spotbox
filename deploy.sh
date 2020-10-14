3426e0b9026f680fdcf1686469669354ffcc7824
Antonio Rodriguez <rodant68@gmail.com>
2020-10-14 14:20:17 +0200

    more about the migration to solidcommunity.net.

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 echo "Building App..."
-sbt test & sbt fullOptJS::webpack
+#sbt test & sbt fullOptJS::webpack
 
 echo "Copying the App Bundle to public dir..."
-cp target/scala-2.12/scalajs-bundler/main/spotbox-opt-bundle.js public/spotbox/js/
+cp target/scala-2.12/scalajs-bundler/main/spotbox-fastopt-bundle.js public/spotbox/js/
 
 echo "Uploading the App to the Solid Server..."
-curl --cookie "connect.sid=${SOLID_COOKIE}" --upload-file index.html https://spotbox.solid.community/index.html
+curl --cookie "connect.sid=${SOLID_COOKIE}" --upload-file index.html https://spotbox.solidcommunity.net/index.html
 echo ":-)"
-curl --cookie "connect.sid=${SOLID_COOKIE}" --upload-file public/spotbox/css/app.css https://spotbox.solid.community/public/spotbox/css/app.css
+curl --cookie "connect.sid=${SOLID_COOKIE}" --upload-file public/spotbox/css/app.css https://spotbox.solidcommunity.net/public/spotbox/css/app.css
 echo ":-)"
-curl --cookie "connect.sid=${SOLID_COOKIE}" --upload-file public/spotbox/images/logo.png https://spotbox.solid.community/public/spotbox/images/logo.png
+curl --cookie "connect.sid=${SOLID_COOKIE}" --upload-file public/spotbox/images/logo.png https://spotbox.solidcommunity.net/public/spotbox/images/logo.png
 echo ":-)"
-curl --cookie "connect.sid=${SOLID_COOKIE}" --upload-file public/spotbox/js/spotbox-opt-bundle.js https://spotbox.solid.community/public/spotbox/js/spotbox-opt-bundle.js
+curl --cookie "connect.sid=${SOLID_COOKIE}" --upload-file public/spotbox/js/spotbox-fastopt-bundle.js https://spotbox.solidcommunity.net/public/spotbox/js/spotbox-opt-bundle.js
 echo "Done :-)"
