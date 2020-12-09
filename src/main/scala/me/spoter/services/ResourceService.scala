e9c83845f36335ba6c1da772424db4e868370d78
Antonio Rodriguez <rodant68@gmail.com>
2020-12-09 12:57:50 +0100

    Correct the POD server URL production.

--- src/main/scala/me/spoter/services/ResourceService.scala
+++ src/main/scala/me/spoter/services/ResourceService.scala
@@ -15,8 +15,8 @@ import scala.scalajs.js
 import scala.scalajs.js.Dynamic
 
 object ResourceService {
-  //private val podServerUrl = "https://spoterme-solid-server.herokuapp.com"
-  private val podServerUrl = "http://localhost:8080"
+  private val podServerUrl = "https://spoterme-solid-server.herokuapp.com"
+  //private val podServerUrl = "http://localhost:8080"
   implicit val sttpBackend: SttpBackend[Future, Nothing, NothingT] = FetchBackend()
 
   private def resourceFrom(iri: IRI, res: Option[js.Object] = None, isPod: Boolean = false): Future[FSResource] = {
